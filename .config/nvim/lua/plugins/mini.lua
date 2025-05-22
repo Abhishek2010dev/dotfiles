e796dc6dabc58438fd0feec8c6796fc060838f48
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-22 12:50:19 +0530

    snacks plugin

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
-		require("mini.indentscope").setup()
 
 		local statusline = require("mini.statusline")
 		statusline.setup({ use_icons = vim.g.have_nerd_font })
@@ -11,29 +10,5 @@ return {
 		statusline.section_location = function()
 			return "%2l:%-2v"
 		end
-	end,
-	init = function()
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"Trouble",
-				"alpha",
-				"dashboard",
-				"fzf",
-				"help",
-				"lazy",
-				"mason",
-				"neo-tree",
-				"notify",
-				"snacks_dashboard",
-				"snacks_notif",
-				"snacks_terminal",
-				"snacks_win",
-				"toggleterm",
-				"trouble",
-			},
-			callback = function()
-				vim.b.miniindentscope_disable = true
-			end,
-		})
 	end,
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -38,7 +38,6 @@ return {
 		},
 
 		spec = {
-			{ "<leader>s", group = "[S]earch" },
 			{ "<leader>t", group = "[T]oggle" },
 			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 		},
