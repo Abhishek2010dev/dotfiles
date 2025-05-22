0b64c0d61b1dd27f5c5bc61e47cd546e7d7b0533
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-22 14:51:27 +0530

    rustaceanvim setup

---
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,27 @@
+return {
+	"mrcjkb/rustaceanvim",
+	ft = { "rust" },
+	opts = {
+		server = {
+			default_settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+						loadOutDirsFromCheck = true,
+						buildScripts = {
+							enable = true,
+						},
+					},
+					procMacro = {
+						enable = true,
+						ignored = {
+							["async-trait"] = { "async_trait" },
+							["napi-derive"] = { "napi" },
+							["async-recursion"] = { "async_recursion" },
+						},
+					},
+				},
+			},
+		},
+	},
+}
