52ec87ac80c20c465d5652499b1e8cdff1664c8b
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-24 12:03:37 +0530

    improved lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,52 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					},
+				},
+				lualine_x = {
+					{
+						"diff",
+						symbols = { added = "+", modified = "~", removed = "-" },
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+				},
+				lualine_y = {
+					{ "progress" },
+					{ "location" },
+				},
+				lualine_z = {
+					function()
+						return " " .. os.date("%R")
+					end,
+				},
+			},
+			extensions = { "fzf", "neo-tree" },
+		})
 	end,
 }
