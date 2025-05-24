ee4c851f8e05a37d358a1e1d6888c10bb628fb85
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-24 14:23:55 +0530

    new shortcut to which-key

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,35 +1,35 @@
 return {
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>h",
-				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
-				end
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>h",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
 			end,
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports" },
-				["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-				["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-			},
+			mode = "",
+			desc = "[F]ormat buffer",
 		},
-	}
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			local disable_filetypes = { c = true, cpp = true }
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				return nil
+			else
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			go = { "goimports" },
+			["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+			["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+		},
+	},
+}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,46 +6,6 @@ return {
 			options = {
 				theme = "auto",
 				globalstatus = true,
-				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						"filename",
-						path = 1,
-					},
-					{
-						"diagnostics",
-						symbols = { error = " ", warn = " ", info = " ", hint = " " },
-					},
-				},
-				lualine_x = {
-					{
-						"diff",
-						symbols = { added = "+", modified = "~", removed = "-" },
-						source = function()
-							local gitsigns = vim.b.gitsigns_status_dict
-							if gitsigns then
-								return {
-									added = gitsigns.added,
-									modified = gitsigns.changed,
-									removed = gitsigns.removed,
-								}
-							end
-						end,
-					},
-				},
-				lualine_y = {
-					{ "progress" },
-					{ "location" },
-				},
-				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
-				},
 			},
 			extensions = { "fzf", "neo-tree" },
 		})
